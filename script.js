3ef931e7112968c20dbff7a3a12619c45c7a8ebf
Talha <talhahussain.splitarts@gmail.com>
2024-08-14 03:12:48 +0500

    Created the UI design of the calculator. Also created the basic functions for calculations in javascript file.

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = function(n1 ,n2){
+    return n1 + n2;
+}
+
+const sub = function(n1 ,n2){
+    return n1 - n2;
+}
+
+const mul = function(n1 ,n2){
+    return n1 * n2;
+}
+
+const div = function(n1 ,n2){
+    return n1 / n2;
+}
+
+const firstNumber = undefined;
+const operator = undefined;
+const secondNumber = undefined;
+
+const screen = document.querySelector(".text-display");
